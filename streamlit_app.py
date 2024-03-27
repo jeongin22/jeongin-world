1e88726d386e44302bc847acb147a1e8798567a2
jeongin22 <162396012+jeongin22@users.noreply.github.com>
2024-03-27 02:45:40 +0000

    a

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,10 @@
 
 import streamlit as st
-import matplotlib.pyplot
+import matplotlib.pyplot as plt
 import numpy as np
 
 
 rand=np.random.normal(1,2,size=20)
-fig.ax=plt.subplots()
+fig, ax=plt.subplots()
 ax.hist(rand, bins=15)
 st.pyplot(fig)
